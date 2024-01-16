e10617a52b9d09c7505712d43b60365c9a5f049f
trandanghieu <trandanghieu2432002@gmail.com>
2024-01-16 19:03:13 +0700

    2.DAO and jUnit Test UserDAO

---
+++ src/main/java/com/bookstore/dao/JpaDAO.java
@@ -0,0 +1,19 @@
+package com.bookstore.dao;
+
+import jakarta.persistence.EntityManager;
+
+public class JpaDAO<T> {
+    protected EntityManager entityManager;
+
+    public JpaDAO(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    public T create(T t){
+        entityManager.getTransaction().begin();
+        entityManager.persist(t);
+        entityManager.flush();
+        entityManager.refresh(t);
+        entityManager.getTransaction().commit();
+        return t;
+    }
+}
